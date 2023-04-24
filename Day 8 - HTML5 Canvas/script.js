a850b86a2e261ec21647cf6a1706150c84adb592
ManuKashyap01 <kashyap.manu200201@gmail.com>
2023-04-24 09:40:14 +0530

    day 8 html canvas challenge complete

---
+++ Day 8 - HTML5 Canvas/script.js
@@ -0,0 +1,47 @@
+const canvas=document.querySelector('#canvas')
+const brush=document.querySelector('.brush')
+const erase=document.querySelector('.erase')
+const ctx=canvas.getContext('2d')
+// ctx.strokeStyle=""
+canvas.width=window.innerWidth-50;
+canvas.height=window.innerHeight-80;
+ctx.lineJoin='round';
+ctx.lineCap='round';
+let lastX=0;
+let lastY=0;
+let isDrawing=false;
+isRemove=false;
+ctx.lineWidth=10;
+let isInc=true;
+let hue=0;
+function Draw(e){
+    if(!isDrawing) return;
+    ctx.strokeStyle=`hsl(${hue},100%,50%)`
+    if(isRemove) ctx.strokeStyle='#fff'
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    [lastX,lastY]=[e.offsetX,e.offsetY];
+    hue++;
+    if(ctx.lineWidth>=75 || ctx.lineWidth<=1){
+        isInc=!isInc;
+    }
+    if(isInc) ctx.lineWidth++;
+    else ctx.lineWidth--;
+}
+brush.addEventListener('click',()=>{
+    isRemove=false
+    console.log('brush')
+})
+erase.addEventListener('click',()=>{
+    isRemove=true
+    console.log('erase')
+})
+window.addEventListener('mousedown',(e)=>{
+    [lastX,lastY]=[e.offsetX,e.offsetY];
+    isDrawing=true;
+})
+window.addEventListener('mousemove',Draw)
+window.addEventListener('mouseup',()=>isDrawing=false)
+window.addEventListener('mouseout',()=>isDrawing=false)
